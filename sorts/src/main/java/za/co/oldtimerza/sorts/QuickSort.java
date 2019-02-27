8b3dfc7e015c206be5e69ccb4cd64b2dc89b951e
Bradley Morris <bradleymorrisza@gmail.com>
2019-02-27 20:50:12 +0200

    Fixing project structure

--- sorts/src/main/java/za/co/oldtimerza/sorts/QuickSort.java
+++ sorts/src/main/java/za/co/oldtimerza/sorts/QuickSort.java
@@ -3,7 +3,8 @@ package za.co.oldtimerza.sorts;
 import java.util.List;
 
 public class QuickSort {
-    public List<Integer> sort(List<Integer> sortableList){
+    public List<Integer> sort(List<Integer> sortableList) {
+        Integer pivotIndex = sortableList.size() / 2;
         return sortableList;
     }
 }
