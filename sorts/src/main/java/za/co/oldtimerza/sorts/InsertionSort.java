ac8c333da23f686debf72fc25b4748a1eb222520
Gavin Morris <gavin.stitches.morris@gmail.com>
2019-04-06 16:35:01 -0400

    Insertion sort practice
    Signed-off-by: Gavin Morris <gavin.stitches.morris@gmail.com>

--- sorts/src/main/java/za/co/oldtimerza/sorts/InsertionSort.java
+++ sorts/src/main/java/za/co/oldtimerza/sorts/InsertionSort.java
@@ -1,7 +1,14 @@
 package za.co.oldtimerza.sorts;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InsertionSort {
     public List<Integer> sort(List<Integer> sortableList){
+        return insertionSort(sortableList);
+    }
 
+    public List<Integer> insertionSort(List<Integer> sortableList){
+        List<Integer> sortedList = new ArrayList<Integer>();
+        return sortedList;
     }
 }
